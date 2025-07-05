dce91009bd4e72ea0d24a2941ebd7e3ee41b6fb0
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-07-05 08:25:00 -0700

    connection make mongodb

--- back/server.js
+++ back/server.js
@@ -13,3 +13,14 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB Connection Successfully!!!");
+});
