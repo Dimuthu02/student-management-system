93553ca66c6a8a732e7aaf5df9765974065345cf
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-07-05 08:13:03 -0700

    import  dependancies

---
+++ back/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+
+app.use(bodyParser.json());
