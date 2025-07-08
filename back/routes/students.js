771a9bd8ee9c34278d2bc6a086e583311db01eed
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-07-08 10:51:25 -0700

    change error

--- back/routes/students.js
+++ back/routes/students.js
@@ -1,8 +1,9 @@
-const rotuer=require("express").Router();
-let Student = require("../models/student.js");
+const router = require("express").Router();
+
+let Student = require("../model/student");
 // Add a new student
 
-rotuer.route("/add").post((req, res) => {
+router.route("/add").post((req, res) => {
   const name = req.body.name;
   const email = req.body.email;
 
@@ -17,35 +18,35 @@ rotuer.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 // Get all students
-rotuer.route("/").get((req, res) => {
+router.route("/").get((req, res) => {
   Student.find()
     .then((students) => res.json(students))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 // Get a student by ID
-rotuer.route("/:id").get((req, res) => {    
-    Student.findById(req.params.id)
-        .then((student) => res.json(student))
-        .catch((err) => res.status(400).json("Error: " + err));
-    });
-// Update a student by ID   
-rotuer.route("/update/:id").post((req, res) => {
-    Student.findById(req.params.id)
-        .then((student) => {
-            student.name = req.body.name;
-            student.email = req.body.email;
+router.route("/:id").get((req, res) => {
+  Student.findById(req.params.id)
+    .then((student) => res.json(student))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+// Update a student by ID
+router.route("/update/:id").post((req, res) => {
+  Student.findById(req.params.id)
+    .then((student) => {
+      student.name = req.body.name;
+      student.email = req.body.email;
 
-            student.save()
-                .then(() => res.json("Student updated successfully"))
-                .catch((err) => res.status(400).json("Error: " + err));
-        })
+      student
+        .save()
+        .then(() => res.json("Student updated successfully"))
         .catch((err) => res.status(400).json("Error: " + err));
-    });
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 // Delete a student by ID
-rotuer.route("/delete/:id").delete((req, res) => {
-    Student.findByIdAndDelete(req.params.id)
-        .then(() => res.json("Student deleted successfully"))
-        .catch((err) => res.status(400).json("Error: " + err));
-    });
-
-
+router.route("/delete/:id").delete((req, res) => {
+  Student.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Student deleted successfully"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+module.exports = router;
