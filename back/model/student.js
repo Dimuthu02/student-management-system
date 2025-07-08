f70abb93d1d3809807315ee0c471f15137eb2a6d
Dimuthu <Sanjuladimuthu2@gmail.com>
2025-07-08 10:25:14 -0700

    add student schema model

---
+++ back/model/student.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const studentSchema=new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+}, {
+    timestamps: true
+});
